f801ecfd86db18a0cc4346d6e1f5922b2d9cfcfc
hawk2 <joehaven@gmail.com>
2024-02-24 10:58:29 -0800

    Initial Commit

---
+++ FunWithStats/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  FunWithStats
+//
+//  Created by Joseph Craig on 2/24/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
